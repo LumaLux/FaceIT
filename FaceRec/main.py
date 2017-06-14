d3b51a7343a28ed065957757fc538b4c5fb29322
LumaLux <jasper.folkers@student.stenden.com>
2017-06-14 12:15:18 +0200

    Add files via upload

---
+++ FaceRec/main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 11:10:37 2017
+
+@author: pracuser
+"""
+
+import cv2
+import compare
+import os
+import sys
+import select
+import order
+import check
+#from PIL import Image
+#import v4l2capture
+#voor pi...
+
+#video = v4l2capture.Video_device("/dev/video0")
+#video.create_buffers(30)
+#video.queue_all_buffers()
+#video.start()
+#eind voor pi...
+
+cap = cv2.VideoCapture(0)
+#cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 200)
+#cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 200)
+
+#print (cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+#print (cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+i = count = 0;
+
+while True:
+    #voor pi...
+    #select.select((video,), (), ())
+
+    #image_data = video.read_and_queue()
+    #eind voor pi...
+    
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print ("Press Enter to stop process.")
+    print (i)
+    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        check.facecheck()
+        order.facecompare()
+        print ("done")
+        break
+    i += 1
+
+    #frame=cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.cv.CV_LOAD_IMAGE_COLOR)
+    
+    ret, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize=(30,30))
+    
+    print("Found {} faces".format(len(faces)))
+    
+    for(x, y, w, h) in faces:
+        count = count+1
+        crop_img = frame[y:y+h, x:x+w]
+        cv2.imwrite("./temp/%02d" % (count, )+".png", crop_img)
+        newimgname = "%02d" % (count, ) + ".png"
+        compare.compare(newimgname)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ FaceRec/order.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 14:21:40 2017
+
+@author: franktieck
+"""
+
+import face_recognition
+import os
+import time
+
+def facecompare():
+    datetime = time.strftime("%c")
+    klas = "./Processed/inf1/"
+    temp = "./temp/"
+    
+    dirs = os.listdir(klas)
+    i=0
+    if dirs == []:
+        for filename in os.listdir(temp):
+            i+=1
+            os.makedirs(klas + "%02d" % (i, ))
+            os.rename(temp  + filename, klas + "%02d" % (i, ) + "/" + datetime)
+            print("new student made")
+    else:
+        for filename in os.listdir(temp):
+            frank_image = face_recognition.load_image_file(temp + filename)
+            frank_face_encoding = face_recognition.face_encodings(frank_image)[0]
+            searching = True
+            i = 0
+            while i < len(dirs) and searching:
+                dirs2 = os.listdir(klas + dirs[i])
+                unknown_image = face_recognition.load_image_file(klas + dirs[i] + "/" + dirs2[0])
+                unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+                known_faces = [
+                     frank_face_encoding
+                ] 
+                results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+                
+                if results[0]:
+                    os.rename(temp + filename, klas + dirs[i] + "/" + datetime)
+                    searching = False
+                else:
+                    i+=1
+            if searching:
+                os.makedirs(klas + "%02d" % (len(dirs)+1, ))
+                os.rename(temp + filename, klas + "%02d" % (len(dirs)+1, ) + "/" + datetime)
+                print("new student made")
