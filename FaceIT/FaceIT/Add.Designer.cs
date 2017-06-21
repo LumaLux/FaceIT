bc0b8d934528e2442fa6f624322afeea8653ef23
DESKTOP-M254RQQ\frank <franktieck-1996@live.nl>
2017-06-21 09:08:33 +0200

    fix

--- FaceIT/FaceIT/Add.Designer.cs
+++ FaceIT/FaceIT/Add.Designer.cs
@@ -34,16 +34,13 @@
             this.ClassName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.button5 = new System.Windows.Forms.Button();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -76,7 +73,7 @@
             // 
             // ClassName
             // 
-            this.ClassName.Location = new System.Drawing.Point(346, 234);
+            this.ClassName.Location = new System.Drawing.Point(346, 278);
             this.ClassName.Name = "ClassName";
             this.ClassName.Size = new System.Drawing.Size(364, 22);
             this.ClassName.TabIndex = 2;
@@ -86,7 +83,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 20.2F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(426, 181);
+            this.label1.Location = new System.Drawing.Point(426, 225);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 41);
             this.label1.TabIndex = 5;
@@ -97,21 +94,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 20.2F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(359, 338);
+            this.label2.Location = new System.Drawing.Point(359, 382);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(310, 41);
             this.label2.TabIndex = 6;
             this.label2.Text = "Number of students:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Calibri", 20.2F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(370, 440);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(290, 41);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Number of lessons:";
             // 
             // label4
             // 
@@ -126,17 +113,10 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(346, 405);
+            this.numericUpDown1.Location = new System.Drawing.Point(346, 449);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(364, 22);
             this.numericUpDown1.TabIndex = 9;
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(346, 498);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(364, 22);
-            this.numericUpDown2.TabIndex = 10;
             // 
             // button5
             // 
@@ -155,7 +135,7 @@
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(346, 313);
+            this.numericUpDown3.Location = new System.Drawing.Point(346, 357);
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(364, 22);
             this.numericUpDown3.TabIndex = 13;
@@ -165,7 +145,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 20.2F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(454, 259);
+            this.label5.Location = new System.Drawing.Point(454, 303);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(134, 41);
             this.label5.TabIndex = 14;
@@ -181,10 +161,8 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ClassName);
@@ -198,7 +176,6 @@
             this.Load += new System.EventHandler(this.Add_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -212,10 +189,8 @@
         private System.Windows.Forms.TextBox ClassName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.NumericUpDown numericUpDown3;
         private System.Windows.Forms.Label label5;
