ef2147d3f6c499d8ba4df427088887c9db2f2f20
FrenckTieck <franktieck-1996@live.nl>
2017-06-22 08:51:58 +0200

    error messages

--- FaceIT/FaceIT/Add.cs
+++ FaceIT/FaceIT/Add.cs
@@ -42,78 +42,86 @@ namespace FaceIT
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Initialize connection / command
-            MySql.Data.MySqlClient.MySqlConnection conn;
-            MySql.Data.MySqlClient.MySqlCommand cmd;
+			if (string.IsNullOrWhiteSpace (ClassName.Text) || string.IsNullOrWhiteSpace (numericUpDown3.Text) || string.IsNullOrWhiteSpace (numericUpDown1.Text)) {
+				MessageBox.Show ("Fields can't be empty.");
+			} else if (Convert.ToInt32 (numericUpDown3.Text) > 4 || Convert.ToInt32 (numericUpDown3.Text) < 1) {
+				MessageBox.Show ("Period must be between 1 and 4.");
+			} else if (Convert.ToInt32 (numericUpDown1.Text) < 1) {
+				MessageBox.Show ("There must be atleast 1 student.");
+			} else {
+				// Initialize connection / command
+				MySql.Data.MySqlClient.MySqlConnection conn;
+				MySql.Data.MySqlClient.MySqlCommand cmd;
 
-            conn = new MySql.Data.MySqlClient.MySqlConnection();
-            cmd = new MySql.Data.MySqlClient.MySqlCommand();
+				conn = new MySql.Data.MySqlClient.MySqlConnection();
+				cmd = new MySql.Data.MySqlClient.MySqlCommand();
 
-            // Set connection / query
-            conn.ConnectionString = "server=localhost;uid=root;pwd=12345;database=FaceIT;";
-			//conn.ConnectionString = "server=localhost;uid=root;pwd=;database=FaceIT;";
-            string myquerystring = "INSERT INTO klas (KlasNaam, Periode, AantalLeerlingen, AantalLessen) VALUES(@KlasNaam, @Periode, @AantalLeerlingen, 0)";
+				// Set connection / query
+				conn.ConnectionString = "server=localhost;uid=root;pwd=12345;database=FaceIT;";
+				//conn.ConnectionString = "server=localhost;uid=root;pwd=;database=FaceIT;";
+				string myquerystring = "INSERT INTO klas (KlasNaam, Periode, AantalLeerlingen, AantalLessen) VALUES(@KlasNaam, @Periode, @AantalLeerlingen, 0)";
 
-            // Check the connection and the query
-            try
-            {
-                // Open the connection and execute command (query)
-                conn.Open();
-                cmd.Connection = conn;
-
-                cmd.Parameters.AddWithValue("@KlasNaam", ClassName.Text);
-                cmd.Parameters.AddWithValue("@Periode", numericUpDown3.Text);
-                cmd.Parameters.AddWithValue("@AantalLeerlingen", numericUpDown1.Text);
-
-
-                cmd.CommandText = myquerystring;
-                cmd.ExecuteNonQuery();
-				String path = System.Reflection.Assembly.GetEntryAssembly().Location;
-                Console.WriteLine(path);
-                if (path.Contains("/"))
-                {
-                    if (path.EndsWith("Debug/FaceIT.exe"))
-                    {
-                        path = path.Replace("FaceIT/FaceIT/bin/Debug/FaceIT.exe", "FaceRec/Processed/");
-                    }
-                    else if (path.EndsWith("Release/FaceIT.exe"))
-                    {
-                        path = path.Replace("FaceIT/FaceIT/bin/Release/FaceIT.exe", "FaceRec/Processed/");
-                    }
-                    System.IO.Directory.CreateDirectory(path + "/" + ClassName.Text + "-" + numericUpDown3.Text);
-                }
-                else
-                {
-                    if (path.EndsWith("Debug\\FaceIT.exe"))
-                    {
-                        path = path.Replace("FaceIT\\FaceIT\\bin\\Debug\\FaceIT.exe", "FaceRec\\Processed\\");
-                    }
-                    else if (path.EndsWith("Release\\FaceIT.exe"))
-                    {
-                        path = path.Replace("FaceIT\\FaceIT\\bin\\Release\\FaceIT.exe", "FaceRec\\Processed'\\");
-                    }
-                    System.IO.Directory.CreateDirectory(path + "\\" + ClassName.Text + "-" + numericUpDown3.Text);
-                }
-               
-                MessageBox.Show("Class successfully added!",
-                "Success!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-                // Close connection
-                conn.Close();
-                //Refreshing the page
-                new Add().Show();
-                this.Hide();
-
-            }
-            catch (MySql.Data.MySqlClient.MySqlException ex)
-            {
-                // Show error
-				if (ex.Number == 1062) {
-					MessageBox.Show ("Class already exist.");
-				} else 
+				// Check the connection and the query
+				try
 				{
-					MessageBox.Show(ex.Message);
+					// Open the connection and execute command (query)
+					conn.Open();
+					cmd.Connection = conn;
+
+					cmd.Parameters.AddWithValue("@KlasNaam", ClassName.Text);
+					cmd.Parameters.AddWithValue("@Periode", numericUpDown3.Text);
+					cmd.Parameters.AddWithValue("@AantalLeerlingen", numericUpDown1.Text);
+
+
+					cmd.CommandText = myquerystring;
+					cmd.ExecuteNonQuery();
+					String path = System.Reflection.Assembly.GetEntryAssembly().Location;
+					Console.WriteLine(path);
+					if (path.Contains("/"))
+					{
+						if (path.EndsWith("Debug/FaceIT.exe"))
+						{
+							path = path.Replace("FaceIT/FaceIT/bin/Debug/FaceIT.exe", "FaceRec/Processed/");
+						}
+						else if (path.EndsWith("Release/FaceIT.exe"))
+						{
+							path = path.Replace("FaceIT/FaceIT/bin/Release/FaceIT.exe", "FaceRec/Processed/");
+						}
+						System.IO.Directory.CreateDirectory(path + "/" + ClassName.Text + "-" + numericUpDown3.Text);
+					}
+					else
+					{
+						if (path.EndsWith("Debug\\FaceIT.exe"))
+						{
+							path = path.Replace("FaceIT\\FaceIT\\bin\\Debug\\FaceIT.exe", "FaceRec\\Processed\\");
+						}
+						else if (path.EndsWith("Release\\FaceIT.exe"))
+						{
+							path = path.Replace("FaceIT\\FaceIT\\bin\\Release\\FaceIT.exe", "FaceRec\\Processed'\\");
+						}
+						System.IO.Directory.CreateDirectory(path + "\\" + ClassName.Text + "-" + numericUpDown3.Text);
+					}
+
+					MessageBox.Show("Class successfully added!",
+						"Success!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+					// Close connection
+					conn.Close();
+					//Refreshing the page
+					new Add().Show();
+					this.Hide();
+
 				}
-            }
+				catch (MySql.Data.MySqlClient.MySqlException ex)
+				{
+					// Show error
+					if (ex.Number == 1062) {
+						MessageBox.Show ("Class already exist.");
+					} else 
+					{
+						MessageBox.Show(ex.Message);
+					}
+				}
+			}
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
